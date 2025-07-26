6d4c89612d8b10ff571b56122a798ce274fcb24d
RCT <rcastro@uco.es>
2025-07-26 21:17:52 +0200

    Fix urgente: quitar referencia a módulo para que Documenter funcione

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,8 @@
-push!(LOAD_PATH, joinpath(@__DIR__, ".."))  # <-- Esto es obligatorio
-
 using Documenter
-using MSE  # El módulo debe existir en src/MSE.jl
 
 makedocs(
     sitename = "Monitorización de la Salud Estructural (MSE)",
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    modules = [MSE],
     pages = [
         "Inicio" => "index.md"
     ]
