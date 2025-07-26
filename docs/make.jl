48a18db55c29a9adfc8ddad3ea9bc0498c2e5139
RCT <rcastro@uco.es>
2025-07-26 21:09:11 +0200

    Fix: añadir LOAD_PATH para Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,19 @@
+push!(LOAD_PATH, joinpath(@__DIR__, ".."))  # Añade la raíz del repo al LOAD_PATH
+
 using Documenter
-using MSE  # tu módulo principal
+using MSE  # Ahora se podrá cargar desde src/MSE.jl
 
 makedocs(
     sitename = "Monitorización de la Salud Estructural (MSE)",
-    modules = [MSE],
     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    modules = [MSE],
     pages = [
-        "Introducción" => "index.md",
-        "Ejemplos" => "ejemplos.md"
+        "Inicio" => "index.md"
     ]
 )
 
 deploydocs(
     repo = "github.com:me1catrr/MSE.git",
     branch = "gh-pages",
+    devbranch = "main"
 )
